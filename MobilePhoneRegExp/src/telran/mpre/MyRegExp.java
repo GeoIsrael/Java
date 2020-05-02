0209b10bb1734f7846e34af7c21ba02445b6bddd
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-05-02 12:54:46 +0300

    255, ipv4, phone, email, time, color, arithmetic

--- MobilePhoneRegExp/src/telran/mpre/MyRegExp.java
+++ MobilePhoneRegExp/src/telran/mpre/MyRegExp.java
@@ -1,18 +1,64 @@
 package telran.mpre;
 
 public class MyRegExp {
-
-	public static boolean isMobile(String string) {
-		
-		boolean res1 = string.matches("053\\d{7}");
-		boolean res2 = string.matches("[+]972\\d{9}");
-		if (res1||res2) return true;
-		return res2;
-	}
-
-	public static boolean isIp(String string) {
-		return string.matches("(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+    //0-255 equals
+	public static boolean is255(String string) {
+		return string.matches("1?\\d?\\d|2[0-4]\\d|25[0-5]");
 	}
 	
+	//ipv4 equals
+	public static boolean isIpv4(String string) {
+		return string.matches("((1?\\d?\\d|2[0-4]\\d|25[0-5])\\.){3}(1?\\d?\\d|2[0-4]\\d|25[0-5])");
+	}
+	
+	
+	//israel mobile phone equals
+	public static boolean isIsraelMobile(String string) {
+		String regex = "(\\+972-?5[0-8]-?|05[0-8]-?)(\\d-?){7}";
+		return string.matches(regex);
+	}
 
+	public static boolean isEmailMailRu(String string) {
+		String regex = "(\\w)+[@]mail\\.ru";
+		return string.matches(regex);
+	}
+	
+	//isTime 00:00 - 23:59
+	public static boolean isTime(String string) {
+		String regex = "(2[0-3]|[0-1]\\d):[0-5]\\d";
+		return string.matches(regex);
+	}
+	
+	//isColor HEX
+	public static boolean isColor(String string) {
+		String regex = "#[0-9a-fA-F]{6}";
+		return string.matches(regex);
+	}
+	
+	//isArithmetic   
+	public static boolean isArithmetic(String string) {
+		String regex = "\\s*[-]?\\s*\\d+(\\s*[-+*/]\\s*\\d+)*\\s*" ;
+		return string.matches(regex);
+		}
+	
+	
+	
+	
+	
+	
 }
+
+//Pattern pattern = Pattern.compile("\\s*(\\s|,|!|\\.)\\s*");
+
+
+//Разбивка текста по словам=========================================================
+//String text = "FIFA will never regret it";
+//String[] words = text.split("\\s*(\\s|,|!|\\.)\\s*");
+//for(String word : words){
+//    System.out.println(word);
+//}
+
+
+
+
+
--- MobilePhoneRegExp/src/telran/mpre/RegExpTest.java
+++ MobilePhoneRegExp/src/telran/mpre/RegExpTest.java
@@ -7,24 +7,77 @@ import org.junit.Test;
 public class RegExpTest {
 
 	@Test
-	public void IsMobileTest() {
-		assertTrue(MyRegExp.isMobile("0530000000"));
-		assertTrue(MyRegExp.isMobile("0534567822"));
-		assertTrue(MyRegExp.isMobile("+972000003344"));
-		assertTrue(MyRegExp.isMobile("+972000003340"));
+	public void Is255Test() {
+		assertTrue(MyRegExp.is255("1"));
+		assertTrue(MyRegExp.is255("255"));
+		assertTrue(MyRegExp.is255("145"));
+		assertTrue(MyRegExp.is255("76"));
 		
-		assertFalse(MyRegExp.isMobile("05300000d"));
-		assertFalse(MyRegExp.isMobile("+93200000000"));
+		assertFalse(MyRegExp.is255("256"));
+		assertFalse(MyRegExp.is255("566"));
 		
 	}
 	
 	@Test
 	public void IsIpTest() {
-		assertTrue(MyRegExp.isIp("10.10.10.255"));
-		assertTrue(MyRegExp.isIp("8.8.8.8"));
-		assertFalse(MyRegExp.isIp("10."));
-		assertFalse(MyRegExp.isIp("9.4"));
+		assertTrue(MyRegExp.isIpv4("255.255.255.255"));
+		assertTrue(MyRegExp.isIpv4("8.8.8.8"));
+		assertFalse(MyRegExp.isIpv4("10."));
+		assertFalse(MyRegExp.isIpv4("9.4"));
 		
 	}
+	
+	
+	@Test
+	public void isIsraelMobileTest() {
+		assertTrue(MyRegExp.isIsraelMobile("+972531111222"));
+		assertTrue(MyRegExp.isIsraelMobile("0532123456"));
+		assertFalse(MyRegExp.isIsraelMobile("10."));
+		assertFalse(MyRegExp.isIsraelMobile("9.4"));
+		
+	}
+	
+	@Test
+	public void isEmailTest() {
+		assertTrue(MyRegExp.isEmailMailRu("123@mail.ru"));
+		assertTrue(MyRegExp.isEmailMailRu("333@mail.ru"));
+		assertFalse(MyRegExp.isEmailMailRu("10."));
+		assertFalse(MyRegExp.isEmailMailRu("9.4"));
+		
+	}
+	
+	@Test
+	public void isTimeTest() {
+		assertTrue(MyRegExp.isTime("22:12"));
+		assertTrue(MyRegExp.isTime("23:59"));
+		assertTrue(MyRegExp.isTime("00:00"));
+		assertTrue(MyRegExp.isTime("13:12"));
+		
+		assertFalse(MyRegExp.isTime("10:60"));
+		assertFalse(MyRegExp.isTime("24:00"));
+	}
+	
+	
+	@Test
+	public void isColorTest() {
+		assertTrue(MyRegExp.isColor("#aaaaaa"));
+		assertTrue(MyRegExp.isColor("#000000"));
+		assertTrue(MyRegExp.isColor("#935432"));
+		
+		assertFalse(MyRegExp.isColor("#ggghhj"));
+		assertFalse(MyRegExp.isColor("#fffffk"));
+	}
+	
+	@Test
+	public void isArithmeticTest() {
+		assertTrue(MyRegExp.isArithmetic(" - 1 + 2"));
+		assertTrue(MyRegExp.isArithmetic("5 / 5"));
+		assertTrue(MyRegExp.isArithmetic("2-1"));
+		
+		assertFalse(MyRegExp.isArithmetic("4df"));
+		assertFalse(MyRegExp.isArithmetic("5 6"));
+	}
+	
+	
 
 }
