27dc33fb53052ed5abd3a96142bcfa9ae8004ed2
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-05-09 13:22:30 +0300

    STRINGBUILDER VERSION

--- PhotoSelector/src/telran/hw/PhotoSelector.java
+++ PhotoSelector/src/telran/hw/PhotoSelector.java
@@ -11,17 +11,20 @@ public class PhotoSelector {
 		
 		if (pictures.length == 0) return null;
 		
-		String res = new String();
+//		String res = new String();
+		StringBuilder res = new StringBuilder();
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher("");
 
 		for (int i = 0; i < pictures.length; i++) {
 			matcher.reset(pictures[i]);
-		    if (matcher.find()) res += (pictures[i] + DELIMITER); 
+		    if (matcher.find()) res.append(pictures[i]).append(DELIMITER); 
+		    	
+//		    	res += (pictures[i] + DELIMITER); 
 		    }
 		
-		return res.split(";");		
-		
+//		return res.split(";");		
+		return res.toString().split(DELIMITER);
 	}
 	
 }
