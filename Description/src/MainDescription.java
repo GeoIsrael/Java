3bdd0d7fd494cbc2dfc8e9baf46fb2009484d499
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-05-15 11:51:05 +0300

    15.05.2020

---
+++ Description/src/MainDescription.java
@@ -0,0 +1,43 @@
+//=====================================
+//ПАТТЕРНЫ
+//=====================================
+//StringBuilder - более быстный метод чем строки, паттерн работы со временем timestamp. много методов()
+//SplitTrim - два метода для редактирования строк и регулярными выражениями
+//RegexpCalculator - разбивка строк на знаки и цифры RegExp, выполнение действий над ними SWITCH CASE.
+//RegExp - примитивный RegExp, проверка строк
+//Products - создание массива объектов,GET/SET с проверкой, правильные конструкторы, this, getClass, getName 
+//PhotoSelector - работа со списком фотографий, выборка при помощи RegExp, паттерн нанизывания на одну строку с DELIMITER, затем split(). 
+//PasswordCheck - проверка строки на соержание символов '!2jkknddr778'
+//MobilePhoneRegExp - RegExp string.matches(); 
+//MinMaxValues - получение MIN MAX основных типов при помощи представлений
+//MinMaxWrappersPro - работа с args[] и получения их максимальных и минимальных значений через switch case методами оберток Wrapper
+//StringsExamples - примеры работ со строками из интернет
+//Employee - класс Company, Employee, Test. методы компании - fire/hire. аннотация before, тесты (контроль массива Employee)
+//BookProject2 - создание примитивных классов и методом на основе примера книг. наследование, полиморфизм, методы
+//---------
+//
+//
+//Generic
+//MyArray
+//AbstractCW  - компания, три типа менеджеров, интерфейс
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class MainDescription {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
