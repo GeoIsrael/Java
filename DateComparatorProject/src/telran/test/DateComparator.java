fae89dea78e795f902619824353a0ac0ac73a395
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-08-08 07:44:20 +0300

    String "date" comparator example

--- DateComparatorProject/src/telran/test/DateComparator.java
+++ DateComparatorProject/src/telran/test/DateComparator.java
@@ -7,14 +7,14 @@ public class DateComparator implements Comparator<String> {
 	@Override
 	public int compare(String arg0, String arg1) {
 		
-		String res1 = "";
+		String res1 = "";    //две пустые строки
 		String res2 = "";
 		
-		String[] temp1 = arg0.split("-");
+		String[] temp1 = arg0.split("-");     //сплитим входную строку в массив строк
 		String[] temp2 = arg1.split("-");
 		
 		for (int i = temp1.length -1; i >= 0; i--) {
-			res1 += temp1[i];
+			res1 += temp1[i];             //конкатенирую строку (убираю "-") из входа
 			res2 += temp2[i]; 
 		}
 		
