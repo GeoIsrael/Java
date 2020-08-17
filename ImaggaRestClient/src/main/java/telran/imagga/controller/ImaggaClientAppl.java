bf5f182d6148bdc25adac2b514c94d87dbb7d11b
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-08-17 16:21:38 +0300

    Imagga API JSON JAVA SPRING MAVEN example

---
+++ ImaggaRestClient/src/main/java/telran/imagga/controller/ImaggaClientAppl.java
@@ -0,0 +1,38 @@
+package telran.imagga.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import telran.imagga.dto.ResponseDto;
+import telran.imagga.dto.TagDto;
+
+public class ImaggaClientAppl {
+
+	public static void main(String[] args) {
+		RestTemplate restTemplate = new RestTemplate();
+		String url = "https://api.imagga.com/v2/tags";
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Basic YWNjX2MzY2Y2YWNhOTMwMGViMzo2MGQ4NTdhMmY3ODg0NGM4ZTMxNTM4MDYwYjhjZmFmMg==");
+		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
+				.queryParam("image_url", "https://24smi.org/public/media/235x307/person/2017/12/22/4sqqykgn04bo-cheburashka.jpg")
+				.queryParam("language", "ru")
+				.queryParam("limit", 3);
+		RequestEntity<String> request = 
+				new RequestEntity<>(headers, HttpMethod.GET, builder.build().toUri());
+		ResponseEntity<ResponseDto> response = 
+				restTemplate.exchange(request, ResponseDto.class);
+		displayTags(response.getBody().getResult().getTags());
+	}
+
+	private static void displayTags(List<TagDto> tags) {
+		tags.forEach(System.out::println);
+		
+	}
+
+}
