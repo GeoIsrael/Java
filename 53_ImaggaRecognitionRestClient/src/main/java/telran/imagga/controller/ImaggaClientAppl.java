4c75a741b2b839a39836a1154d68fd0d7f168d90
qdH3ErcZg8ZehCdf <MDf4nBNjqrufGnxS@protonmail.com>
2020-09-20 13:16:04 +0300

    Imaga Recognition Rest Java client

--- 53_ImaggaRecognitionRestClient/src/main/java/telran/imagga/controller/ImaggaClientAppl.java
+++ 53_ImaggaRecognitionRestClient/src/main/java/telran/imagga/controller/ImaggaClientAppl.java
@@ -10,31 +10,28 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import telran.imagga.dto.ResponseDto;
-import telran.imagga.dto.BackgroundColorDto;
-import telran.imagga.dto.ColorDto;
+import telran.imagga.dto.TagDto;
 
-public class ImaggaColorAppl {
+public class ImaggaClientAppl {
 
 	public static void main(String[] args) {
 		RestTemplate restTemplate = new RestTemplate();
-		String url = "https://api.imagga.com/v2/colors";
+		String url = "https://api.imagga.com/v2/tags";
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("Authorization", "Basic YWNjXzc4ZjA5OWY5ZmY5NzIxMjphYTY0YjcxMzgwMTE5NmVkYTkxNDRlZjk1ZDU2MGNkMw==");
 		UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(url)
 				.queryParam("image_url", "https://24smi.org/public/media/235x307/person/2017/12/22/4sqqykgn04bo-cheburashka.jpg")
 				.queryParam("language", "ru")
-				.queryParam("limit", 5);
+				.queryParam("limit", 4);
 		RequestEntity<String> request = 
 				new RequestEntity<>(headers, HttpMethod.GET, builder.build().toUri());
 		ResponseEntity<ResponseDto> response = 
 				restTemplate.exchange(request, ResponseDto.class);
-		
-		
-		displayColors(response.getBody().getResult().getColors().getBackground_colors());
+		displayTags(response.getBody().getResult().getTags());
 	}
 
-	private static void displayColors(List<BackgroundColorDto> list) {
-		list.forEach(System.out::println);
+	private static void displayTags(List<TagDto> tags) {
+		tags.forEach(System.out::println);
 		
 	}
 
